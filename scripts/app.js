77ce8185f478882f38b63ffab03949f42e5a6487
Exadra37 <exadra37@gmail.com>
2016-10-22 10:41:43 +0100

    Fix path to service worker.

--- scripts/app.js
+++ scripts/app.js
@@ -368,7 +368,7 @@
   // TODO add service worker code here
   if ('serviceWorker' in navigator) {
       navigator.serviceWorker
-               .register('./playground/weather/service-worker.js')
+               .register('service-worker.js')
                .then(function() { console.log('Service Worker Registered'); });
   }
 })();
